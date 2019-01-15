287c4b768b5a4a47de25911ae5d3808b6faf7dd6
Malika <malika.baglan@gmail.com>
2019-01-15 16:01:03 -0600

    MediApp

---
+++ POCMediaApp/POCMediaApp.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.Permissions;
+
+namespace POCMediaApp.Droid
+{
+    [Activity(Label = "POCMediaApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)         {             PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);         }
+    }
+}
